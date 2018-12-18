1768c43fee1899740e37dbdee29d94af7a38c9d4
Teja Adda <32438978+saddagarla2016@users.noreply.github.com>
2018-12-18 16:29:42 -0500

    Add files via upload

---
+++ pleaseeeeeeeeeeemicro1.ino
@@ -0,0 +1,77 @@
+#include <ESP8266WiFi.h>
+
+#include <FireBaseArduino.h>
+
+
+// Set these to run example.
+
+#define FIREBASE_HOST "Look at Firebase Console ( this is using realtime database and not cloud database ) (name of the link for the child and parents are in firebase)"
+
+#define FIREBASE_AUTH "Secreat key (in service accounts in your firebase account)"
+
+#define WIFI_SSID "Wifi Router Name"
+
+#define WIFI_PASSWORD "Router Password"
+
+
+
+void setup() {
+
+
+
+Serial.begin(9600);
+
+WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+Serial.print("connecting");
+
+while (WiFi.status() != WL_CONNECTED) {
+
+Serial.print(".");
+
+delay(500);
+
+}
+
+Serial.println();
+
+Serial.print("connected: ");
+
+Serial.println(WiFi.localIP());
+
+Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+
+}
+
+void loop() {
+
+if(Firebase.getInt("LEDStatus"))
+
+{
+
+digitalWrite(LED,HIGH);
+
+}
+
+else
+
+{
+
+digitalWrite(LED,LOW);
+
+}
+
+if (Firebase.failed()) // Check for errors {
+
+Serial.print("setting /number failed:");
+
+Serial.println(Firebase.error());
+
+return;
+
+}
+
+delay(1000);
+
+}
